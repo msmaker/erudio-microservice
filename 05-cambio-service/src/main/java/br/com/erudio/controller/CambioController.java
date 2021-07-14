df11bc8aa13dd56880a27db0ac1fa248e45db8ae
Marco Silva <marcosilva.tec@gamil.com>
2021-07-14 17:02:42 -0300

    setting up DB MySQL

--- 05-cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ 05-cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.erudio.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import br.com.erudio.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+	
+	@Autowired
+	private Environment environment;
 
 	// http://localhost:8000/cambio-service/5/USD/BRL
 	@GetMapping(value="{amount}/{from}/{to}")
@@ -20,7 +25,10 @@ public class CambioController {
 			@PathVariable("from")String from,
 			@PathVariable("to")String to
 			) {
+		
+		var port = environment.getProperty("local.server.port");
+		
 		return new Cambio(
-				1L,from,to,BigDecimal.ONE,BigDecimal.ONE,"PORT 8000");
+				1L,from,to,BigDecimal.ONE,BigDecimal.ONE,port);
 	}
 }
