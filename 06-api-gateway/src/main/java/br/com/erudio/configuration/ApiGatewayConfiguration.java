9a68eae9a4e80b8be45ecf3c227e57c542437222
Marco Silva <marcosilva.tec@gamil.com>
2021-07-15 23:43:28 -0300

    Swagger openapi gateway

--- 06-api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
+++ 06-api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
@@ -11,11 +11,6 @@ public class ApiGatewayConfiguration {
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p -> p.path("/get")
-							.filters(f->f
-									.addRequestHeader("Hello", "World")
-							        .addRequestParameter("Hello", "World"))
-							.uri("http://httpbin.org:80"))
 				.route(p -> p.path("/cambio-service/**")
 						   .uri("lb://cambio-service"))
 				.route(p -> p.path("/book-service/**")
