73f65d83dd15156f7c12c6470d71b46c939f7236
Marco Silva <marcosilva.tec@gamil.com>
2021-07-15 22:46:54 -0300

    Bulkhead

--- 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,7 +17,8 @@ public class FooBarController {
 	@GetMapping("/foo-bar")
 	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
-	@RateLimiter(name = "default")
+	//@RateLimiter(name = "default")
+	@Bulkhead(name = "default")
 	public String fooBar() {
 		logger.info("Request foo-bar is received");
 		//var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
