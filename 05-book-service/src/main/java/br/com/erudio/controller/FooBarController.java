62fbdf43fc41b61ac5364c79547a78a7163026bb
Marco Silva <marcosilva.tec@gamil.com>
2021-07-15 22:44:17 -0300

    Rate Limit

--- 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -5,9 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 @RequestMapping("book-service")
@@ -16,12 +15,14 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("/foo-bar")
-	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	@RateLimiter(name = "default")
 	public String fooBar() {
 		logger.info("Request foo-bar is received");
-		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-		// return "Foo-bar!!!";
-		return response.getBody();
+		//var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+	    return "Foo-bar!!!";
+		//return response.getBody();
 	}
 
 	public String fallbackMethod(Exception ex) {
